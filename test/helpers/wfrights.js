40bded54565510efaa6e8fe915ceff6f1dfafb5e
Alex <alexz@windeveloper.com>
2020-08-14 01:59:05 +0200

    Added new contants and helpers

--- test/helpers/wfrights.js
+++ test/helpers/wfrights.js
@@ -1,3 +1,5 @@
+const BigNumber = require('bignumber.js');
+
 const WFRights = {
     INIT:    0,    // Start Workflow by submitting 1st document set version
     APPROVE: 1,    // Approve the current document set version
@@ -6,6 +8,42 @@ const WFRights = {
     ABORT:   4     // Abort workflow
 }
 
+const WFMode = {
+    UNINIT:   0,    //State Engine Unintialized
+    RUNNING:  1,    //State Engine Running
+    COMPLETE: 2,    //State Engine Ended Success
+    ABORTED:  3     //State Entine Ended Aborted
+}
+
+const WFFlags = {
+    REQUIRED: 1,    //Document is required
+    PUBLIC:   2     //Document is to be published
+}
+
+function myPad(number, length) {
+    var str = '' + number;
+    while (str.length < length) {
+        str = '0' + str;
+    }
+    return str;
+}
+
+function makeDocID(id, doctype) { 
+    let str = "0x" + myPad(id.toString(16), 8)+ myPad(doctype.toString(16), 8);
+    console.log(str);
+    return BigNumber(str); 
+}
+
+function makeDocSet(loLimit, hiLimit, flags) {
+    let str = "0x" + myPad(flags.toString(16), 8) + myPad(hiLimit.toString(16), 8) + myPad(loLimit.toString(16), 8);
+    console.log(str);
+    return BigNumber(str); 
+}
+
 module.exports = {
-    WFRights
+    WFRights,
+    WFMode,
+    WFFlags,
+    makeDocID,
+    makeDocSet
 }
