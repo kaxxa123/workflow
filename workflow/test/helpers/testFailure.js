e43d00ac47b082d4fda4756fe213bdc0aa596bfb
Alex <alex@windeveloper.com>
2021-03-22 20:52:42 +0100

    Updated testing of failed transactions

--- workflow/test/helpers/testFailure.js
+++ workflow/test/helpers/testFailure.js
@@ -2,15 +2,18 @@
 async function testFail(name, errMsg, lambda) 
 {
     console.log("Testing Failure of: " + name + "()...");
+    let hasFailed = true;
     try {
         await lambda();
-        assert(false, "Should have failed!");
+        hasFailed = false;
     }
     catch(err) {
         if (!err.message.includes(errMsg))
                 assert(false, "Unexpected error: " + err.message);
         else    console.log("Error Confirmed: " + err.message);
     }
+
+    assert(hasFailed, "Should have failed!");
 }
 
 module.exports = {
